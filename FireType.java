195f8445cf14eb4836e22802f9c722d406864242
katch <90023551+kctviernesto@users.noreply.github.com>
2022-11-16 08:46:24 +0800

    Create FireType.java

---
+++ FireType.java
@@ -0,0 +1,26 @@
+public class FireType extends Monster {
+    public FireType(String name, int hp, int base) {
+      super(name, "fire", "grass", "water", hp, base);
+      
+      atk *= 1.3;
+      def *= 0.7;
+    }
+
+    @Override
+    public void special() {
+      super.special();
+
+      atk += 2;
+      hp -= (maxHP * 0.1);
+
+      if (hp < 0) { // caps at 0 hp
+        hp = 0;
+      }
+
+      System.out.println(name + "'s attack increased by 2, but its HP is reduced by 10% of its max HP, making it " + hp + "!");
+
+      if (hp == 0) {
+        System.out.println(name + " fainted.");
+      }
+    }
+}
